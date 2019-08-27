13d1980297ad9d83ddb3b075fb3e23d576d517cc
chenjianwen <chenjianwen@coralglobal.cn>
2019-08-27 18:30:06 +0800

    service

---
+++ shiro-web/src/main/java/com/chenjianwen/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.chenjianwen.service.impl;
+
+import com.chenjianwen.service.PermissionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: <br>
+ * @Date: Created in 2019/8/27 <br>
+ * @Author: chenjianwen
+ */
+@Service
+public class PermissionServiceImpl implements PermissionService {
+}
---
+++ shiro-web/src/main/java/com/chenjianwen/service/impl/RoleServiceImpl.java
@@ -0,0 +1,13 @@
+package com.chenjianwen.service.impl;
+
+import com.chenjianwen.service.RoleService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description: <br>
+ * @Date: Created in 2019/8/27 <br>
+ * @Author: chenjianwen
+ */
+@Service
+public class RoleServiceImpl implements RoleService {
+}
